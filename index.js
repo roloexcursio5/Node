feeaa71ef60e0039959741e95a9afd220c4d5792
Ale <roloexcursio5@gmail.com>
2022-11-04 12:35:06 -0300

    2 pruebo pasar un json

--- index.js
+++ index.js
@@ -2,8 +2,34 @@ const express = require('express');
 const	app = express();
 require('dotenv').config();
 
-app.all('/', (req, res) => {
-      res.status(200).send("<p>asdgfasgafds"+process.env.TEST+"</p>");
+//API
+app.get('/', (req, res) => {
+/*  res.header('Access-Control-Allow-Origin', '*');
+  async function main(){
+    await connectToMongo();
+    return await coleccion.find({}).sort({"titulo": 1})}
+  main()
+    .then((series) => {	 */
+      res.status(200).send({"titulo": "dale", "year":2020});
+/*    })
+    .catch(console.error)
+    .finally(() => mongoose.connection.close());*/
 });
+
+/*
+//API INDIVIDUAL
+app.get('/:id', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  async function main(){
+    await connectToMongo();
+    return await coleccion.findOne({"id":req.params.id})}
+  main()
+    .then((serie) => {	 
+      res.status(200).send(serie);
+    })
+    .catch(console.error)
+    .finally(() => mongoose.connection.close());
+});*/
+
 
 app.listen(8080);
