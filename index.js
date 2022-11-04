e6acb39650ccbfa2b32245561c057dfb6aa1b93a
Ale <roloexcursio5@gmail.com>
2022-11-04 12:41:26 -0300

    3 pruebo conexion con mongodb

--- index.js
+++ index.js
@@ -2,18 +2,38 @@ const express = require('express');
 const	app = express();
 require('dotenv').config();
 
+// importo mongoose y genero una función asíncrona para usar luego
+const mongoose = require("mongoose");
+const connectToMongo = async () => {
+  await mongoose.connect(process.env.SECRETSESSION);
+  return mongoose;
+};  // me hizo sacar todos los parámetros pq desde la versión 6 están deprecados (no sé que impacto tiene eso). Asi funciona
+
+// se genera el esquema
+const serieSchema = new mongoose.Schema({
+      id: Number,
+      titulo: String,
+      protagonista: String,
+      pais: String
+    });
+    
+    const tabla = "series"  // es el nombre de la coleccion
+
+// se define la coleccion usando el esquema
+const coleccion = mongoose.model(tabla, serieSchema);
+
 //API
 app.get('/', (req, res) => {
-/*  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
   async function main(){
     await connectToMongo();
     return await coleccion.find({}).sort({"titulo": 1})}
   main()
-    .then((series) => {	 */
-      res.status(200).send({"titulo": "dale", "year":2020});
-/*    })
+    .then((series) => {	 
+      res.status(200).send(series);
+    })
     .catch(console.error)
-    .finally(() => mongoose.connection.close());*/
+    .finally(() => mongoose.connection.close());
 });
 
 /*
